c0c77ed5eae5c8360d32fb52180136ee948ce73c
SmonSE <68323384+SmonSE@users.noreply.github.com>
2021-12-03 14:53:05 +0100

    replace

--- stations/streamContent.py
+++ stations/streamContent.py
@@ -24,7 +24,7 @@ def get_streamContent_url():
         metadata_length = struct.unpack('B', response.read(1))[0] * 16  # length byte
         metadata = response.read(metadata_length).rstrip(b'\0')
         LOG.info(f'MetaData length: {metadata_length}')
-        LOG.info(f'MetaData: {metadata}')
+        #LOG.info(f'MetaData: {metadata}')
 
         # extract title from the metadata
         m = re.search(br"StreamTitle='([^']*)';", metadata)
@@ -37,7 +37,7 @@ def get_streamContent_url():
             LOG.info("This is an info level log message.")
         
     #LOG.info(f'Title: {title.decode()}')
-    mp3Artist = title.decode(encoding, errors='ignore')
+    mp3Artist = title.decode(encoding, errors='replace')
     LOG.info("### DREI ###")
 
     return mp3Artist
