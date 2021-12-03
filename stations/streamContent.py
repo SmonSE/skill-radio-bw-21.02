f582e2842dc65b6ef58c63fb1de82e402dd9ad3d
SmonSE <68323384+SmonSE@users.noreply.github.com>
2021-12-03 12:05:45 +0100

    fix

--- stations/streamContent.py
+++ stations/streamContent.py
@@ -1,6 +1,7 @@
 # https://stackoverflow.com/questions/35102278/python-3-get-song-name-from-internet-radio-stream
 
-from __future__ import print_function
+from __init__ import print_function
+from urllib.request import urlopen
 import re
 import struct
 import sys
