46410fa97977a14776e622a17492394928f90e59
SmonSE <68323384+SmonSE@users.noreply.github.com>
2021-12-03 19:42:59 +0100

    match fix

--- stations/streamContent.py
+++ stations/streamContent.py
@@ -27,7 +27,7 @@ def get_streamContent_url():
 
         # extract title from the metadata
         #m = re.search(br"StreamTitle='([^']*)';", metadata)
-        m = re.search(br"match=b='([^']*)';", metadata)
+        m = re.search(br"match=b'([^']*)';", metadata)
         LOG.info(f'MetaData m: {m}') 
 
         if m:
