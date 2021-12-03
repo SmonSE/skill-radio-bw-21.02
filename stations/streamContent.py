62f1a6bf2795d263895825b4dd8cbe37e0c36432
SmonSE <68323384+SmonSE@users.noreply.github.com>
2021-12-03 21:48:10 +0100

    dasding meta works

--- stations/streamContent.py
+++ stations/streamContent.py
@@ -1,4 +1,6 @@
-# https://stackoverflow.com/questions/35102278/python-3-get-song-name-from-internet-radio-stream
+# 
+#
+#
 
 from __future__ import print_function
 from mycroft.util import LOG
@@ -24,22 +26,21 @@ def get_streamContent_url():
         response.read(metaint)  # skip to metadata
         metadata_length = struct.unpack('B', response.read(1))[0] * 16  # length byte
         metadata = response.read(metadata_length).rstrip(b'\0')
-        LOG.info(f'MetaData length: {metadata_length}')
+        #LOG.info(f'MetaData length: {metadata_length}')
 
         # extract title from the metadata
-        m = re.search(br"='([^']*)';", metadata)
-        #m = re.search(br"player__song'([^']*)';", metadata)
-        LOG.info(f'MetaData m: {m}') 
+        #MetaData m: <_sre.SRE_Match object; span=(11, 51), match=b"='Bitch better have my money - Rihanna';">
+        meta = re.search(br"'([^']*)';", metadata)
+        LOG.info(f'MetaData: {meta}') 
 
-        if m:
-         title = m.group(1)
+        if meta:
+         title = meta.group(1)
          if title:
                break
         else: 
-            sys.exit('no title found')
-            LOG.info("no title found.")
+            sys.exit('no meta data found')
+            LOG.info("no meta data found.")
         
     #mp3Artist = title.decode(encoding, errors='replace')
     LOG.info(f'#META#: {title}') 
-
     return str(title)
