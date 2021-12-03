0b944337d118b5954ee5187e7af497d791001162
SmonSE <68323384+SmonSE@users.noreply.github.com>
2021-12-03 20:07:13 +0100

    trest

--- stations/streamContent.py
+++ stations/streamContent.py
@@ -12,7 +12,8 @@ except ImportError:  # Python 3
 
 def get_streamContent_url():
 
-    url = 'https://www.radio.de/s/bigfm'  # radio stream
+    #url = 'https://liveradio.swr.de/sw282p3/dasding/play.mp3'  # radio stream
+    url = 'http://streams.bigfm.de/bigfm-bw-128-mp3'  # radio stream
     encoding = 'latin1' # default: iso-8859-1 for mp3 and utf-8 for ogg streams
     request = urllib2.Request(url, headers={'Icy-MetaData': 1})  # request metadata
     response = urllib2.urlopen(request)
