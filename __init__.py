b57761ebb589f635f11cb3beefd69bb9ac9ebc04
SmonSE <68323384+SmonSE@users.noreply.github.com>
2021-12-08 20:22:20 +0100

    test

--- __init__.py
+++ __init__.py
@@ -200,8 +200,6 @@ class RadioSkill(CommonPlaySkill):
         self.stop()
         # Speak intro while downloading in background
         self.speak_dialog('radio', data={"from": station.full_name})
-        # Update GUI permanent  -> is working
-        self.schedule_repeating_event(self.update_station_content(station), None, 15)
         self._play_station(station)
         self.last_station_played = station
         self.enable_intent('restart_playback')
@@ -280,6 +278,9 @@ class RadioSkill(CommonPlaySkill):
             #get_streamContent_url()
             artistTitle = find_metaData_url(media_url)
             self.log.info(f'Artist from _play_station: {artistTitle}')
+
+            # Update GUI permanent  -> is working
+            self.schedule_repeating_event(self.update_station_content(station), None, 15)
 
             # Ensure announcement of station has finished before playing
             wait_while_speaking()
