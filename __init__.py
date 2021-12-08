1434140d9d89c50b9e96da0d95846bd347d279e6
SmonSE <68323384+SmonSE@users.noreply.github.com>
2021-12-08 08:32:40 +0100

    fix

--- __init__.py
+++ __init__.py
@@ -246,7 +246,7 @@ class RadioSkill(CommonPlaySkill):
     def update_station_content(self, station: BaseStation):
         """Update the station content to gui permantent."""
         self.log.info("Update GUI every 10 seconds")
-        self.log.info(f'Station acronym: {station.acronym}')   # -> fail missed station 
+        #self.log.info(f'Station acronym: {station.acronym}')   # -> fail missed station 
 
         #media_content = stations.media_url
         #self.log.info(f'Load media content: {media_content}')
